c2de2c036f9567596a47f4c22880a435a53dc556
Jakub Jankiewicz <jcubic@onet.pl>
2022-03-30 22:42:47 +0200

    first commit

---
+++ MemoryDB.js
@@ -0,0 +1,71 @@
+
+ class MemoryDB {
+     constructor(data) {
+         if (data) {
+             this._store = this.load(data);
+         } else {
+             this._store = new Map();
+         }
+     }
+     saveSuperblock(superblock) {
+         this._store.set('!root', superblock);
+     }
+     loadSuperblock() {
+         return this._store.get('!root');
+     }
+     readFile(inode) {
+         return this._store.get(inode);
+     }
+     writeFile(inode, data) {
+         return this._store.set(inode, data);
+     }
+     unlink(inode) {
+         return this._store.delete(inode);
+     }
+     wipe() {
+         this._store = new Map();
+     }
+     close() { }
+     dump() {
+         const sb = this._store.get('!root');
+         const result = {
+             superBlock: seralizeSuperBlock(sb),
+             inodes: Array.from(this._store.entries()).filter(([key, value]) => {
+                 return key !== '!root';
+             }).map(([key, value]) => {
+                 return [key, Array.from(value)];
+             })
+         };
+         return result;
+     }
+     load(data) {
+         const result = new Map();
+         result.set('!root', unserializeSuperBlock(data.superBlock));
+         data.inodes.map(([inode, data]) => {
+             result.set(inode, Uint8Array.from(data));
+         });
+         return result;
+     }
+ }
+
+ function seralizeSuperBlock(sb) {
+     const arr = Array.from(sb.entries());
+
+     return arr.map(([key, value]) => {
+         if (value instanceof Map) {
+             return [key, seralizeSuperBlock(value)];
+         }
+         return [key, value];
+     });
+ }
+
+ function unserializeSuperBlock(arr) {
+     return new Map(arr.map(([key, value]) => {
+         if (value instanceof Array) {
+             return [key, unserializeSuperBlock(value)];
+         }
+         return [key, value];
+     }));
+ }
+ 
+ export default MemoryDB;
---
+++ make-image.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+import { promises as fs } from 'fs';
+import process from 'process';
+import path from 'path';
+import pako from 'pako';
+import LightningFS from '../../lightning-fs/src/index.js';
+import MemoryDB from './MemoryDB.js';
+import lily from '@jcubic/lily';
+
+import 'fake-indexeddb/auto.js';
+global.navigator = {};
+
+// ref: https://gist.github.com/lovasoa/8691344
+async function* walk(root, dir = '') {
+    const current_dir = path.join(root, dir);
+    for await (const d of await fs.opendir(current_dir)) {
+        const directory = d.isDirectory();
+        const entry = path.join(dir, d.name);
+        const name = directory ? entry + '/' : entry;
+        yield name;
+        if (directory) yield* walk(root, entry);
+    }
+}
+
+const db = new MemoryDB();
+const { promises: lfs } = new LightningFS('fs', { db });
+
+const main = async options => {
+    const { d: root, f: output, v: verbose } = options;
+    for await (let fpath of walk(root)) {
+        if (verbose) {
+            console.log(fpath);
+        }
+        const name = '/' + fpath;
+        if (name.endsWith('/')) {
+            await lfs.mkdir(name.replace(/\/$/, ''));
+        } else {
+            const file = await fs.readFile(path.join(root, fpath));
+            await lfs.writeFile(name, file);
+        }
+    }
+    await lfs.flush();
+    const dump = pako.deflate(JSON.stringify(db.dump()));
+    await fs.writeFile(output, dump);
+}
+
+const options = lily(process.argv.slice(2));
+if (!(options.d && options.f)) {
+    console.error('make-image.js -d <directory> -f <output file>');
+} else {
+    main(options);
+}
